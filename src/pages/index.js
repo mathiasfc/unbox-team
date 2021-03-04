0c3401ab46c4003cbca8e33aa614de7c6e0e487c
mathiasfc <mathias.falci@gmail.com>
2021-03-04 18:30:01 -0300

    :sparkles: added next seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,38 @@
 import HomeTemplate from "templates/Home";
+import { NextSeo } from "next-seo";
 import { initializeApollo, addApolloState } from "../lib/apolloClient";
 import { MEMBERS_QUERY } from "../graphql/queries.gql";
 
 const Home = ({ members }) => {
-  return <HomeTemplate members={members} />;
+  const title = "Time Unbox";
+  const description = "Site com todos os integrantes do time da unbox :)";
+
+  return (
+    <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical="https://unbox-team.vercel.app/"
+        openGraph={{
+          type: "website",
+          locale: "pt_BR",
+          url: "https://unbox-team.vercel.app/",
+          site_name: title,
+          title,
+          description,
+          images: [
+            {
+              url: "/images/icons/unbox.png",
+              width: 256,
+              height: 256,
+              alt: "Logo da Unbox",
+            },
+          ],
+        }}
+      />
+      <HomeTemplate members={members} />
+    </>
+  );
 };
 
 export async function getServerSideProps() {
