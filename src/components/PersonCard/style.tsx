73116c291079965fe93bb9621b66656169b14fea
mathiasfc <mathias.falci@gmail.com>
2021-03-05 11:55:36 -0300

    alternate background color

--- src/components/PersonCard/style.tsx
+++ src/components/PersonCard/style.tsx
@@ -15,6 +15,10 @@ export const PersonItem = styled.li`
   &:nth-child(2n + 1) {
     background-color: #f78be8;
   }
+
+  &:nth-child(4n + 3) {
+    grid-column: 2;
+  }
 `;
 
 export const PersonHeader = styled.div`
